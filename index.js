2b3e0edc27c80562c938fc0383829e06e20cab58
Madhura Mihiranga <it19177410@my.sliit.lk>
2022-05-16 18:57:10 +0530

    ys

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('#GOTA GO HOME !!!!!!!!XXXXX!!')
+  res.send('#GOTA GO HOME !!!!!!!!YYYYY')
 })
 
 app.listen(port, () => {
